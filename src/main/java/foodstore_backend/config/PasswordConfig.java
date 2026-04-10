56ff276eccb47fef1bd685f5ef209d4a30a2b287
Damián Ignacio Nogueira <damian.nogueira@outlook.com>
2026-04-10 18:29:38 -0300

    feat: se reemplaza SHA-256 password encoder por BCrypt (pedido por HU)

--- src/main/java/foodstore_backend/config/PasswordConfig.java
+++ src/main/java/foodstore_backend/config/PasswordConfig.java
@@ -2,6 +2,7 @@ package foodstore_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 // Configuración del encoder de contraseñas
@@ -10,6 +11,6 @@ public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Sha256PasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
